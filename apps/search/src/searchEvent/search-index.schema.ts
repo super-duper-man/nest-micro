b0c5bbfd6f4587d8e08f8b6a155ab32e70dc1db5
MohammadReza Fouladi <m.r.fouladi7@gmail.com>
2026-01-05 08:28:43 +0330

    search product schema

---
+++ apps/search/src/searchEvent/search-index.schema.ts
@@ -0,0 +1,24 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { HydratedDocument } from "mongoose";
+
+export type SearchProductDocument = HydratedDocument<SearchProduct>;
+
+@Schema({ timestamps: true })
+export class SearchProduct {
+    @Prop({ required: true, unique: true, index: true })
+    productId: string;
+
+    @Prop({ required: true })
+    name: string;
+    @Prop({ required: true })
+    normalizeText: string;
+
+
+    @Prop({ required: true, enum: ['DRAFT', 'ACTIVE'], default: 'DRAFT' })
+    status: 'DRAFT' | 'ACTIVE';
+
+    @Prop({required: true})
+    price: number;
+}
+
+export const SearchProductSchema = SchemaFactory.createForClass(SearchProduct);
